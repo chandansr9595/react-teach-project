eac64e6fa06ac1522ceacd96325521622c13a28c
chandansr9595 <chandan95sr@gmail.com>
2020-11-27 09:21:52 +0530

    created stream create

--- create-twitch/src/components/streams/StreamCreate.js
+++ create-twitch/src/components/streams/StreamCreate.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Field,  reduxForm} from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
+class StreamCreate extends React.Component {
+
+    renderInputBox = (formProps) => {
+        return <div className="field">
+            <label>{formProps.label}</label>
+            <input {...formProps.input} autoComplete='off'/>
+        </div>
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        console.log('submitted')
+    }
+
+    render() {
+        return (
+            <form className="ui form" onSubmit={(event) => this.onSubmit(event)}>
+                <Field 
+                    name="title" 
+                    component={this.renderInputBox}
+                    label="Enter Title"
+                />
+                <Field 
+                    name="description" 
+                    component={this.renderInputBox}
+                    label="Enter Description"
+                />
+                <button className="ui button primary">Submit</button>
+            </form>
+        )
+    }
 }
 
-export default StreamCreate;
+
+export default reduxForm({
+    form: 'CREATE_STREAM'
+})(StreamCreate);
