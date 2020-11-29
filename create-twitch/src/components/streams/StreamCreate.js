3cd7a3b32775f8b51202d64572800b91e1f66488
chandansr9595 <chandan95sr@gmail.com>
2020-11-29 23:31:37 +0530

    created api for react

--- create-twitch/src/components/streams/StreamCreate.js
+++ create-twitch/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field,  reduxForm} from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions'; 
 
 class StreamCreate extends React.Component {
 
@@ -14,7 +16,7 @@ class StreamCreate extends React.Component {
     }
 
     onSubmit = (formValues) => {
-        console.log('submitted : ', formValues);
+        this.props.createStream(formValues);
     }
 
     render() {
@@ -51,7 +53,9 @@ const validateForm = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'CREATE_STREAM',
     validate: validateForm
 })(StreamCreate);
+
+export default connect(null, {createStream})(formWrapped);
--- create-twitch/src/index.js
+++ create-twitch/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import reduxThunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 import { Provider } from 'react-redux';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducers, composeEnhancers(applyMiddleware())
+  reducers, composeEnhancers(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(
